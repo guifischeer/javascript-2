100fadd0088655b94e047a81c23434a721c593da
guifischeer <guilherme_florent@estudante.sesisenai.org.br>
2025-03-21 12:55:56 -0300

    2

--- script.js
+++ script.js
@@ -7,4 +7,36 @@ function verificarSinal(n1) {
       console.log("O número é zero");
     }
   }
-  console.log (10)
+  console.log (verificarSinal(3))
+
+  ///////////////////////////////////////////////
+
+function converterTemperatura(celsius) {
+    return (celsius * 9) / 5 + 32;
+  }
+  console.log (converterTemperatura(30))
+
+///////////////////////////////////////////////
+
+
+
+
+///////////////////////////////////////////////
+
+let Palavra = prompt("Digite uma palavra:");
+function contarVogais(Palavra){
+
+    const vogais = "aeiouAEIOU";
+    let contador = 0;    
+
+    for (let letra of Palavra)
+    if(vogais.includes(letra)){
+        contador++;
+
+    }
+    console.log(contador);
+}
+contarVogais(Palavra);
+
+///////////////////////////////////////////////
+
