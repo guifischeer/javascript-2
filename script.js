ee21cdd15b7f5afd4b8e4e675c727e613078281e
guifischeer <guilherme_florent@estudante.sesisenai.org.br>
2025-03-21 11:45:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function verificarSinal(n1) {
+    if (n1 > 0) {
+      console.log("O número é positivo");
+    } else if (n1 < 0) {
+      console.log("O número é negativo");
+    } else {
+      console.log("O número é zero");
+    }
+  }
+  console.log (10)
